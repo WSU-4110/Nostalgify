018f75dca7f3a54b0675148a720f80cd9d98db19
kasataya <kassemataya0@gmail.com>
2024-02-02 00:04:35 -0500

    Creating the expo application and downloaded the needed files for the project

---
+++ NostalGify/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi Kevin</Text>
+      <Text style={styles.text}>Let's Get This Party Started</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
