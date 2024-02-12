2c87f43630811d957540cdbad672f97f1c31d609
Parham635 <Pmarzban42@gmail.com>
2024-02-11 20:27:04 -0500

    test

--- NostalGify/App.js
+++ NostalGify/App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import * as Font from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, CheckBox } from "react-native-elements";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync(MaterialIcons.font);
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hi Kevin</Text>
-      <Text style={styles.text}>Let's Get This Party Started</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Button
+        raised
+        icon={{ name: "cached", color: "white" }}
+        title="Wassup kevin"
+      />
+      <CheckBox
+        title="dont click this"
+        checked={checked}
+        onPress={() => setChecked((checked) => !checked)}
+      />
     </View>
   );
 }
@@ -13,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
