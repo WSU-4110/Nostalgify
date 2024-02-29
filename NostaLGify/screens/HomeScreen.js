5b35dd33b44b64218e61928ee4e5ec61c2106c97
kevinjzh <gw7937@wayne.edu>
2024-02-29 17:21:34 -0500

    home screen w/ icons

--- NostaLGify/screens/HomeScreen.js
+++ NostaLGify/screens/HomeScreen.js
@@ -1,13 +1,72 @@
-import {StyleSheet, Text, View} from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
 
 const HomeScreen = () => {
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={{backgroundColor: '#cca2b7'}}>
+            <View style={{ flexDirection: 'row', marginTop: 50}}>
+                <AntDesign
+                    name="hearto"
+                    size={100}
+                    color="black"
+                    style={{
+                        marginLeft: 30,
+                        marginTop: 50,
+                    }}
+                />
+
+                <Text style={{ marginTop: 80, marginLeft: 30, fontSize: 30, fontWeight: "bold" }}>Liked Songs</Text>
+            </View>
+            
+            <View style={{ flexDirection: 'row' }}>
+                <MaterialCommunityIcons
+                    name="playlist-music-outline"
+                    size={100}
+                    color="black"
+                    style={{
+                        marginLeft: 30,
+                        marginTop: 50
+                     }}
+                />
+
+                <Text style={{ marginTop: 80, marginLeft: 30, fontSize: 30, fontWeight: "bold" }}>Playlists</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row' }}>
+                <FontAwesome6
+                    name="record-vinyl"
+                    size={100}
+                    color="black"
+                    style={{
+                        marginLeft: 30,
+                        marginTop: 50
+                    }}
+                />
+
+                <Text style={{ marginTop: 80, marginLeft: 30, fontSize: 30, fontWeight: "bold" }}>Albums</Text>
+            </View>
+
+            <View style={{ flexDirection: 'row', marginBottom: 120}}>
+                <MaterialCommunityIcons
+                    name="microphone-variant"
+                    size={100}
+                    color="black"
+                    style={{
+                        marginLeft: 30,
+                        marginTop: 50
+                    }}
+                />
+
+                <Text style={{ marginTop: 80, marginLeft: 30, fontSize: 30, fontWeight: "bold" }}>Artists</Text>
+            </View>
+            
         </View>
     )
 }
+
 
 export default HomeScreen
 const styles = StyleSheet.create({})
