de8a83f59e0213e97bb569ecd1941a2eeee62e3d
kasataya <kassemataya0@gmail.com>
2024-02-29 19:38:55 -0500

    changining log in screen

--- NostalGify/screens/LoginScreen.js
+++ NostalGify/screens/LoginScreen.js
@@ -1,60 +1,9 @@
 import { StyleSheet, Text, View, SafeAreaView, Pressable } from 'react-native';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Entypo } from '@expo/vector-icons';
-import * as AppAuth from "expo-auth-session"
-import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const LoginScreen = () => {
-    const navigation = useNavigation();
-    useEffect(() =>{                                         
-        const checkTokenValidity = async () => {
-            // verify using token and expiration date. log if expired (?)
-            const accessToken = await AsyncStorage.getItem("token");
-            const expirationDate = await AsyncStorage.getItem("expirationDate");
-            console.log("access token", accessToken);
-            console.log("expiration date", expirationDate);
-
-            if(accessToken && expirationDate){
-                // checking if token and expdate are good based on current time
-                const currentTime = Date.now();
-                if(currentTime < parseInt(expirationDate)){
-                    // token is still valid in this case
-                    navigation.replace("HomeScreen");
-                } else{
-                    // token is expired, so remove from async storage
-                    AsyncStorage.removeItem("token");
-                    AsyncStorage.removeItem("expirationDate");
-
-                }
-            }
-        }
-        checkTokenValidity();
-    }, [])
-    async function authenticate (){
-        const config = {
-            issuer:"https://accounts.spotify.com", 
-            clientId:"df72319c2a67411985723932d7d0ac4b",
-            scopes:[
-                "user-read-email",
-                "user-library-read",
-                "user-read-recently-played",
-                "user-top-read",
-                "playlist-read-private",
-                "playlist-read-collaborative",
-                "playlist-modify-public"
-            ],
-            redirectUrl:"exp://localhost:8081/--/spotify-auth-callback"
-        }
-        const result = await AppAuth.authAsync(config);
-        console.log(result);
-        if(result.accessToken) {
-            const expirationDate = new Date(result.accessTokenExpirationDate).getTime();
-            AsyncStorage.setItem("token",result.accessToken);
-            AsyncStorage.setItem("expirationDate", expirationDate.toString());
-            navigation.navigate("HomeScreen")
-        }
-    }
     return (
         <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
             <SafeAreaView>
