db8e77ff3d4e86a1de3364ac48703114a161321c
Parham635 <Pmarzban42@gmail.com>
2024-03-22 01:00:32 -0400

    Added implementation of pulling access token from async via sign out

--- NostaLGify/screens/SettingsScreen.js
+++ NostaLGify/screens/SettingsScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
-const SettingsScreen = ({ navigation }) => {
-  const signOut = () => {
+const SettingsScreen = () => {
+  const navigation = useNavigation();
+
+  const signOut = async () => {
     console.log('Signing out of Spotify');
+    console.log("Before: ",await AsyncStorage.getItem('accessToken'));
+    await AsyncStorage.removeItem('accessToken');
+    console.log("After: ",await AsyncStorage.getItem('accessToken'));
+    navigation.navigate('Login');
   };
 
   const changeTheme = () => {
     console.log('Changing theme');
+    // Implement theme changing logic here
   };
 
   return (
