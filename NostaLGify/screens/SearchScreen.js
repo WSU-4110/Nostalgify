f67876c1038b0e5235fa5ae741b6fbc6dc571fb8
Kevin Zheng <kev@wayne.edu>
2024-04-11 21:46:48 -0400

    getPlaylists

--- NostaLGify/screens/SearchScreen.js
+++ NostaLGify/screens/SearchScreen.js
@@ -1,14 +1,67 @@
 import { StyleSheet, Text, View, TextInput, FlatList, TouchableOpacity, Image, Pressable, ScrollView } from 'react-native';
-import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from 'react';
+
+
+
+async function fetchWebApi(endpoint, method, body, token) {
+    const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      let url = `https://api.spotify.com/${endpoint}`;
+      if (method === 'GET' && body) {
+        // If it's a GET request and there's a body, encode it into the URL
+        const queryParams = new URLSearchParams(body).toString();
+        url += `?${queryParams}`;
+      }
+      const res = await fetch(url, {
+        headers,
+        method,
+        body: method !== 'GET' ? JSON.stringify(body) : undefined, // Only include body for non-GET requests
+      });
+      return await res.json();
+  }
+
+  async function getPlaylists(token) {
+    return await fetchWebApi(
+      'v1/me/playlists?limit=50&offset=0', 'GET', null, token
+    );
+  }
 
 const SearchScreen = () => {
     const navigation = useNavigation();
+    
+    const [playlists, setPlaylists] = useState(null);
+
+    useEffect(() => {
+        fetchPlaylists();
+      }, []);
+    
+      const fetchPlaylists = async () => {
+        try {
+          const accessToken = await AsyncStorage.getItem('accessToken');
+          if (accessToken) {
+            const playlistsData = await getPlaylists(accessToken);
+            if (playlistsData?.items) {
+              setPlaylists(playlistsData.items);
+              console.log()
+            } else {
+              setPlaylists(null);
+            }
+          } else {
+            console.error('Access token not found in AsyncStorage');
+          }
+        } catch (error) {
+          console.error('Error fetching current track:', error);
+        }
+      };
+
     return (   
         <View style={{ backgroundColor: '#cca2b7', flex: 1, paddingTop: 20 }}>
             <Text style={{ marginLeft: 10, fontSize:30, fontWeight: "bold", color: "#583b55"}}> Your Library </Text>
@@ -89,7 +142,13 @@ const SearchScreen = () => {
                     elevation: 3,
                     }}
                 >
-                    <Feather name="square" size={70} color="#7f6581" />
+
+                    {playlists && playlists[0] && playlists[0].images && playlists[0].images.length > 0 && (
+                        <Image
+                            source={{ uri: playlists[0].images[0].url }}
+                            style={{ width: 70, height: 70 }}
+                        />
+                    )}
 
                     <Text style={{ color: "#6a5874", fontSize: 13, fontWeight: "bold" }}>
                         Playlist 1
