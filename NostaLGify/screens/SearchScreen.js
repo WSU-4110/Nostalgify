00280f8b0a4d18d28e77e214208a0e62dab561f8
kevinjzh <gw7937@wayne.edu>
2024-04-16 00:41:34 -0400

    Populating library w/ playlists

--- NostaLGify/screens/SearchScreen.js
+++ NostaLGify/screens/SearchScreen.js
@@ -7,65 +7,65 @@ import { LinearGradient } from 'expo-linear-gradient';
 import { Feather } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState, useEffect } from 'react';
+import PlaylistItem from "../components/PlaylistItem";
 
 
 
 async function fetchWebApi(endpoint, method, body, token) {
     const headers = {
         Authorization: `Bearer ${token}`,
-      };
-      let url = `https://api.spotify.com/${endpoint}`;
-      if (method === 'GET' && body) {
+    };
+    let url = `https://api.spotify.com/${endpoint}`;
+    if (method === 'GET' && body) {
         // If it's a GET request and there's a body, encode it into the URL
         const queryParams = new URLSearchParams(body).toString();
         url += `?${queryParams}`;
-      }
-      const res = await fetch(url, {
+    }
+    const res = await fetch(url, {
         headers,
         method,
         body: method !== 'GET' ? JSON.stringify(body) : undefined, // Only include body for non-GET requests
-      });
-      return await res.json();
-  }
+    });
+    return await res.json();
+}
 
-  async function getPlaylists(token) {
+async function getPlaylists(token) {
     return await fetchWebApi(
-      'v1/me/playlists?limit=50&offset=0', 'GET', null, token
+        'v1/me/playlists?offset=0&limit=50', 'GET', null, token
     );
-  }
+}
 
 const SearchScreen = () => {
     const navigation = useNavigation();
-    
-    const [playlists, setPlaylists] = useState(null);
+
+    const [playlists, setPlaylists] = useState([]);
 
     useEffect(() => {
         fetchPlaylists();
-      }, []);
-    
-      const fetchPlaylists = async () => {
-        try {
-          const accessToken = await AsyncStorage.getItem('accessToken');
-          if (accessToken) {
-            const playlistsData = await getPlaylists(accessToken);
-            if (playlistsData?.items) {
-              setPlaylists(playlistsData.items);
-              console.log()
-            } else {
-              setPlaylists(null);
-            }
-          } else {
-            console.error('Access token not found in AsyncStorage');
-          }
-        } catch (error) {
-          console.error('Error fetching current track:', error);
-        }
-      };
+    }, []);
 
-    return (   
+    const fetchPlaylists = async () => {
+        try {
+            const accessToken = await AsyncStorage.getItem('accessToken');
+            if (accessToken) {
+                const playlistsData = await getPlaylists(accessToken);
+                if (playlistsData?.items) {
+                    setPlaylists(playlistsData.items);
+                } else {
+                    setPlaylists(null);
+                }
+            } else {
+                console.error('Access token not found in AsyncStorage');
+            }
+        } catch (error) {
+            console.error('Error fetching current track:', error);
+        }
+    };
+
+    return (
         <View style={{ backgroundColor: '#cca2b7', flex: 1, paddingTop: 20 }}>
-            <Text style={{ marginLeft: 10, fontSize:30, fontWeight: "bold", color: "#583b55"}}> Your Library </Text>
-            <View style={{backgroundColor: '#cca2b7', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 20, paddingTop: 20}}>
+            <Text style={{ marginLeft: 10, fontSize: 30, fontWeight: "bold", color: "#583b55" }}> Your Library </Text>
+            <View style={{ backgroundColor: '#cca2b7', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 20, paddingTop: 20 }}>
                 <View style={{ flexDirection: 'row' }}>
                     <MaterialCommunityIcons
                         name="playlist-music-outline"
@@ -93,35 +93,35 @@ const SearchScreen = () => {
                         color="#6a5874"
                     />
 
-                    <Text style={{ color: "#6a5874", fontSize: 15, fontWeight: "bold"}}>Artists</Text>
+                    <Text style={{ color: "#6a5874", fontSize: 15, fontWeight: "bold" }}>Artists</Text>
                 </View>
             </View>
 
             <ScrollView>
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 15,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginLeft: 10,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <LinearGradient colors={["#33006F", "#FFFFFF"]}>
-                    <Pressable
-                        style={{
-                        width: 55,
-                        height: 55,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        }}
-                    >
-                        <AntDesign name="heart" size={24} color="#ab8ca4" />
-                    </Pressable>
+                        <Pressable
+                            style={{
+                                width: 50,
+                                height: 50,
+                                justifyContent: "center",
+                                alignItems: "center",
+                            }}
+                        >
+                            <AntDesign name="heart" size={24} color="#ab8ca4" />
+                        </Pressable>
                     </LinearGradient>
 
                     <Text style={{ color: "#6a5874", fontSize: 13, fontWeight: "bold" }}>
@@ -129,17 +129,27 @@ const SearchScreen = () => {
                     </Text>
                 </Pressable>
 
+                <FlatList
+                    data={playlists}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                        <PlaylistItem item={item} />
+                    )}
+                />
+
+
+                {/*
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
 
@@ -157,15 +167,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -177,15 +187,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -197,15 +207,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -217,15 +227,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -237,15 +247,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -257,15 +267,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -277,15 +287,15 @@ const SearchScreen = () => {
 
                 <Pressable
                     style={{
-                    marginTop: 20,
-                    flexDirection: "row",
-                    alignItems: "center",
-                    gap: 10,
-                    flex: 1,
-                    marginHorizontal: 7,
-                    backgroundColor: "#cca2b7",
-                    borderRadius: 4,
-                    elevation: 3,
+                        marginTop: 20,
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 10,
+                        flex: 1,
+                        marginHorizontal: 7,
+                        backgroundColor: "#cca2b7",
+                        borderRadius: 4,
+                        elevation: 3,
                     }}
                 >
                     <Feather name="square" size={70} color="#7f6581" />
@@ -294,8 +304,8 @@ const SearchScreen = () => {
                         Playlist 8
                     </Text>
                 </Pressable>
+                */}
 
-               
 
             </ScrollView>
         </View>
