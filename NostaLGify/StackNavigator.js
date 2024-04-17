0e247d9cc2a4318a14957b61de20dfde3d041f98
a-cyphi <acyoherw@gmail.com>
2024-04-17 17:07:08 -0400

    updated nav bar

--- NostaLGify/StackNavigator.js
+++ NostaLGify/StackNavigator.js
@@ -21,19 +21,15 @@ const BottomTabs = () => {
     <Tab.Navigator
       screenOptions={{
         tabBarStyle: {
-          backgroundColor: 'rgba(0,0,0,0.5)',
+          backgroundColor: '#413244',
           position: 'absolute',
           bottom: 0,
           left: 0,
           right: 0,
-          shadowOpacity: 4,
-          shadowRadius: 4,
           elevation: 4,
-          shadowOffset: {
-            width: 0,
-            height: -4,
-          },
-          borderTopWidth: 0,
+          borderTopWidth: 6,
+          borderTopColor: '#725876',
+          paddingTop: 10
         },
       }}>
       <Tab.Screen
