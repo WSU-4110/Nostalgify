4631b98772de522b22abe08e066c554f4f2a82e5
kasataya <kassemataya0@gmail.com>
2024-04-17 22:08:38 -0400

    ewsdfg

--- NostaLGify/firebase-config.js
+++ NostaLGify/firebase-config.js
@@ -1,45 +1,34 @@
-import { initializeApp, getApp, getApps } from 'firebase/app';
-import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll } from "firebase/storage";
+import { initializeApp, getApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import {
     FIREBASE_API_KEY,
     FIREBASE_APP_ID,
-    FIREBASE_STORAGE_BUCKET,
-    FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID
+    FIREBASE_PROJECT_ID,
+    FIREBASE_AUTH_DOMAIN
 } from "@env";
 
 // Initialize Firebase
 const firebaseConfig = {
     apiKey: FIREBASE_API_KEY,
-    storageBucket: FIREBASE_STORAGE_BUCKET,
-    appId: FIREBASE_APP_ID,
-    projectID: FIREBASE_PROJECT_ID,
-    authDomain: FIREBASE_AUTH_DOMAIN
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    appId: FIREBASE_APP_ID
 };
 
-if (getApps().length === 0) {
+if (!getApp().length) {
     initializeApp(firebaseConfig);
 }
 
-const fbApp = getApp();
-const fbStorage = getStorage();
+const firestore = getFirestore();
 
 const listFiles = async () => {
-    const storage = getStorage();
-    const listRef = ref(storage, 'images');
-
     try {
-        const listResp = await listAll(listRef);
-        const filePromises = listResp.items.map(async (item) => {
-            const downloadURL = await getDownloadURL(item);
-            return {
-                name: item.name,
-                uri: downloadURL,
-            };
-        });
-
-        // Wait for all file promises to resolve
-        return await Promise.all(filePromises);
+        const querySnapshot = await getDocs(collection(firestore, 'photosConnectedToSongs'));
+        const files = querySnapshot.docs.map(doc => ({
+            name: doc.id,
+            uri: doc.data().uri // Adjust this line based on your document structure
+        }));
+        return files;
     } catch (error) {
         console.error('Error listing files:', error);
         return [];
@@ -75,8 +64,6 @@ const uploadToFirebase = async (uri, name, onprogress) => {
 };
 
 export {
-    fbApp,
-    fbStorage,
     uploadToFirebase,
     listFiles
 };
