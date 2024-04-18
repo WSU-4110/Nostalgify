74472fa894dccfcb029d573645a5676f968e4a7a
kasataya <kassemataya0@gmail.com>
2024-04-17 22:02:03 -0400

    g

--- NostaLGify/firebase-config.js
+++ NostaLGify/firebase-config.js
@@ -1,7 +1,12 @@
-import { getApp } from 'firebase/app';
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { getFirestore, collection, addDoc, serverTimestamp, query, where, getDocs } from 'firebase/firestore';
-import { FIREBASE_API_KEY, FIREBASE_APP_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID } from "@env";
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL, listAll } from "firebase/storage";
+import {
+    FIREBASE_API_KEY,
+    FIREBASE_APP_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID
+} from "@env";
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -12,25 +17,29 @@ const firebaseConfig = {
     authDomain: FIREBASE_AUTH_DOMAIN
 };
 
+if (getApps().length === 0) {
+    initializeApp(firebaseConfig);
+}
+
 const fbApp = getApp();
 const fbStorage = getStorage();
-const firestore = getFirestore(fbApp);
 
 const listFiles = async () => {
+    const storage = getStorage();
+    const listRef = ref(storage, 'images');
+
     try {
-        const imageRef = collection(firestore, 'photosConnectedToSongs', 'DL72C7HLRAL6QBW6WHUL');
-        const q = query(imageRef, where('FieldValue', '==', 'Image'));
-        const querySnapshot = await getDocs(q);
-        
-        const fileList = [];
-        querySnapshot.forEach((doc) => {
-            fileList.push({
-                name: doc.id,
-                uri: doc.data().FieldValue,
-            });
+        const listResp = await listAll(listRef);
+        const filePromises = listResp.items.map(async (item) => {
+            const downloadURL = await getDownloadURL(item);
+            return {
+                name: item.name,
+                uri: downloadURL,
+            };
         });
 
-        return fileList;
+        // Wait for all file promises to resolve
+        return await Promise.all(filePromises);
     } catch (error) {
         console.error('Error listing files:', error);
         return [];
@@ -38,23 +47,31 @@ const listFiles = async () => {
 };
 
 const uploadToFirebase = async (uri, name, onprogress) => {
-    try {
-        const response = await fetch(uri);
-        const blob = await response.blob();
-        
-        const imageRef = collection(firestore, 'photosConnectedToSongs', 'DL72C7HLRAL6QBW6WHUL');
-        
-        // Upload image to Firestore
-        const docRef = await addDoc(imageRef, {
-            FieldValue: name, // Assuming name is the image URL or any relevant field
-            timestamp: serverTimestamp(), // Store upload timestamp
-        });
+    const fetchResponse = await fetch(uri);
+    const theBlob = await fetchResponse.blob();
 
-        return docRef.id; // Return document ID if needed
-    } catch (error) {
-        console.error('Error uploading image to Firestore:', error);
-        throw error;
-    }
+    const imageRef = ref(getStorage(), `images/${name}`);
+
+    const uploadTask = uploadBytesResumable(imageRef, theBlob);
+
+    return new Promise((resolve, reject) => {
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                onprogress && onprogress(progress);
+            },
+            (error) => {
+                reject(error)
+            },
+            async () => {
+                const DownloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+                resolve({
+                    DownloadURL,
+                    metadata: uploadTask.snapshot.metadata,
+                })
+            }
+        );
+    });
 };
 
 export {
