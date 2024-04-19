f3bdd1374ee617bc283af5e6e6a72b4d1f217ef8
Parham635 <Pmarzban42@gmail.com>
2024-04-19 00:01:41 -0400

    added more test cases

--- NostaLGify/screens/LoginScreen.test.js
+++ NostaLGify/screens/LoginScreen.test.js
@@ -22,7 +22,57 @@ jest.mock("@react-navigation/native", () => {
 
 describe('LoginScreen component', () => {
     it('renders without crashing', () => {
-        render(<LoginScreen />);
+      render(<LoginScreen />);
     });
-
-});
+  
+    it('displays the app name correctly', () => {
+      const { getByText } = render(<LoginScreen />);
+      expect(getByText('Nostalgify')).toBeTruthy();
+    });
+  
+    it('contains a "Sign In with Spotify" button', () => {
+      const { getByText } = render(<LoginScreen />);
+      expect(getByText('Sign In with Spotify')).toBeTruthy();
+    });
+  
+    it('contains a "Guest" button', () => {
+      const { getByText } = render(<LoginScreen />);
+      expect(getByText('Guest')).toBeTruthy();
+    });
+  
+    it('navigates to the Main screen when "Guest" button is pressed', async () => {
+      const mockNavigate = jest.fn();
+      jest.mock('@react-navigation/native', () => ({
+        ...jest.requireActual('@react-navigation/native'),
+        useNavigation: () => ({
+          navigate: mockNavigate,
+        }),
+      }));
+      const { getByText } = render(<LoginScreen />);
+      const guestButton = getByText('Guest');
+      fireEvent.press(guestButton);
+      await waitFor(() => {
+        expect(mockNavigate).toHaveBeenCalledWith('Main');
+      });
+    });
+  
+    it('calls promptAsync when "Sign In with Spotify" button is pressed', async () => {
+      const mockPromptAsync = jest.fn();
+      jest.mock('@react-navigation/native', () => ({
+        ...jest.requireActual('@react-navigation/native'),
+        useNavigation: () => ({
+          navigate: jest.fn(),
+        }),
+      }));
+      jest.mock('expo-auth-session', () => ({
+        ...jest.requireActual('expo-auth-session'),
+        useAuthRequest: () => [null, null, mockPromptAsync],
+      }));
+      const { getByText } = render(<LoginScreen />);
+      const spotifyButton = getByText('Sign In with Spotify');
+      fireEvent.press(spotifyButton);
+      await waitFor(() => {
+        expect(mockPromptAsync).toHaveBeenCalled();
+      });
+    });
+  });
