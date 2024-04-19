c2be2abd4693ec3470b9d08124cf9bafa9ede6ff
Parham635 <Pmarzban42@gmail.com>
2024-04-18 23:33:21 -0400

    unit testing

--- NostaLGify/babel.config.js
+++ NostaLGify/babel.config.js
@@ -2,9 +2,10 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    "plugins": [
+    plugins: [
       ["module:react-native-dotenv"]
     ]
   };
 };
 
+module.exports.presets = ['@babel/preset-env'];
