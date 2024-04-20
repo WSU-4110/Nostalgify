d75970ce09fceb70b6941b60095be30bda0c1d5d
Kevin Zheng <kev@wayne.edu>
2024-04-20 15:58:17 -0400

    UI update, tags

--- NostaLGify/screens/SongScreen.js
+++ NostaLGify/screens/SongScreen.js
@@ -153,7 +153,6 @@ export default SongScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
     },
     galleryContainer: {
         position: 'absolute',
