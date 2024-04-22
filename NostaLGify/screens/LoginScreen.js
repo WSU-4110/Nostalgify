e76859c916d9aa2e5eb068734cc90649746adaf0
kevinjzh <kev@wayne.edu>
2024-04-21 21:47:46 -0400

    packages

--- NostaLGify/screens/LoginScreen.js
+++ NostaLGify/screens/LoginScreen.js
@@ -45,8 +45,8 @@ const LoginScreen = () => {
     // Define the authentication request
     const [request, response, promptAsync] = useAuthRequest(
         {
-            clientId: 'e1316c7324f34e9baad599caa68aadd2', // Parham's Client
-            //clientId: 'df72319c2a67411985723932d7d0ac4b', // Kevin's Client:
+            //clientId: 'e1316c7324f34e9baad599caa68aadd2', // Parham's Client
+            clientId: '06335027a9a548db97dae585531719dd', // Kevin's Client:
             scopes: [
                 'user-read-email',
                 'user-read-private',
@@ -73,10 +73,10 @@ const LoginScreen = () => {
                 // Exchange the authorization code for an access token
                 const tokenEndpoint = 'https://accounts.spotify.com/api/token';
                 const redirectUri = makeRedirectUri({ scheme: 'nostalgify', native: "exp://localhost:8081",native: "http://localhost:8081"  });
-                const clientId = 'e1316c7324f34e9baad599caa68aadd2'; // Parham's client
-                //const clientId = 'df72319c2a67411985723932d7d0ac4b'; // Kevin's client
-                const clientSecret = '6a8d779d312a437f947755e810610357'; // Parham's client
-                //const clientSecret = '9623064c109349db979a2a7f6c2f791a'; // Kevin's client
+                //const clientId = 'e1316c7324f34e9baad599caa68aadd2'; // Parham's client
+                const clientId = '06335027a9a548db97dae585531719dd'; // Kevin's client
+                //const clientSecret = '6a8d779d312a437f947755e810610357'; // Parham's client
+                const clientSecret = '2e8ac880929b45a1a39930265e28b5ae'; // Kevin's client
                 const requestBody = {
                     grant_type: 'authorization_code',
                     code,
